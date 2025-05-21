f2eba4c28bceabc971212644b7967ce8d7c77f46
Prabhat D Rawal <156101772+prabhatdrawal@users.noreply.github.com>
2025-05-21 14:52:17 +0545

    Delete FULL_Adder.py

--- FULL_Adder.py
+++
@@ -1,8 +0,0 @@
-A = int(input("enter binary bit value 0 or 1"))
-B = int(input("enter binary bit value 0 or 1"))
-C = int(input("enter binary bit value 0 or 1"))
-
-
-sum = (A^B^C)
-carry = (A&B | B&C | C&A)
-print(f"sum = {sum}"f" and carry = {carry}") 
